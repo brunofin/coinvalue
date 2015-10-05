e213150c85651d8e022d0f71a21f13f187c439ee
Bruno Finger <bruno.finger12@gmail.com>
2015-10-05 19:44:02 +0200

    #1 - Expose providers on a public API.

--- backend/coinvalue-backend/apiv1/views.py
+++ backend/coinvalue-backend/apiv1/views.py
@@ -1,16 +1,22 @@
+# -*- coding: utf-8 -*-
+
 from django.shortcuts import render
 from currency.models import Provider
 from django.http.response import HttpResponse
 from django.core import serializers
-import http
+import http, simplejson as json
 
 
 def providers(request):
     if request.method == 'GET':
         # returns a simplified list of providers
+        list = []
         providers = Provider.objects.all()
+        
+        for provider in providers:
+            list.append(provider.to_json())
+        
         return HttpResponse(status=http.client.OK, 
-            content=serializers.serialize('json', providers),
-            content_type='application/json')
+            content=json.dumps(list), content_type="application/json")
     else:
         return HttpResponse(status=http.client.METHOD_NOT_ALLOWED)
--- backend/coinvalue-backend/currency/models.py
+++ backend/coinvalue-backend/currency/models.py
@@ -1,8 +1,17 @@
+# -*- coding: utf-8 -*-
+
 from django.db import models
+import json
 
 class DigitalCurrency(models.Model):
     id = models.CharField(max_length=5, primary_key=True)
     name = models.CharField(max_length=25)
+    
+    def to_json(self):
+        return {
+            'id': self.id,
+            'name': self.name
+        }
 
 class Provider(models.Model):
     name = models.CharField(max_length=255)
@@ -11,6 +20,28 @@ class Provider(models.Model):
     refCurrency = models.CharField(max_length=5)
     digitalCurrency = models.ForeignKey(DigitalCurrency)
     
+    
+    def to_json(self):
+        return {
+            'id': self.pk,
+            'name': self.name,
+            'url': self.url,
+            'ticker': self.ticker,
+            'refCurrency': self.refCurrency,
+            'digitalCurrency': self.digitalCurrency.id
+        }
+    
+    """
+    def to_JSON(self):
+        return { 'a': 1}
+        def _try(o): 
+            try:
+                return o.__dict__
+            except:
+                return str(o)
+        return json.dumps(self, default=lambda o: _try(o), sort_keys=True, indent=0, separators=(',',':')).replace('\n', '')"""
+    
+    
 class History(models.Model):
     provider = models.ForeignKey(Provider)
     date = models.DateTimeField()
@@ -18,3 +49,14 @@ class History(models.Model):
     sell = models.PositiveSmallIntegerField()
     high = models.PositiveSmallIntegerField()
     low = models.PositiveSmallIntegerField()
+    
+    def to_json(self):
+        return {
+            'id': self.pk,
+            'providerId': self.provider.pk,
+            'date': self.date,
+            'buy': self.buy,
+            'sell': self.sell,
+            'high': self.high,
+            'low': self.low
+        }
