e379b0e473f54eb9ca497791ec997da52856d949
Bruno Finger <bruno.finger12@gmail.com>
2015-10-10 16:35:57 +0200

    Small full-stack example/manual test

--- frontend/app/scripts/app/Coinvalue.js
+++ frontend/app/scripts/app/Coinvalue.js
@@ -1,4 +1,11 @@
 (function(angular) {
     var app = angular.module('Coinvalue', ['Providers']);
     
+    app.controller('teste', ['$scope', 'ProvidersDAO', function($scope, ProvidersDAO) {
+	$scope.providers = null;
+	
+	ProvidersDAO.list().success(function(data) {
+	    $scope.providers = (data);
+	});
+    }]);
 })(window.angular);
