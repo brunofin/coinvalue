33e9e531be734018214aaddbdf57f0c7e6c9a8bc
Bruno Finger <bruno.finger12@gmail.com>
2015-10-10 16:35:23 +0200

    #3 - Integrate History to the frontend

--- frontend/app/scripts/app/Providers.js
+++ frontend/app/scripts/app/Providers.js
@@ -2,19 +2,35 @@
     var app = angular.module('Providers', []);
     
     app.service('ProvidersDAO', ['$http', function($http) {
+	var base = 'http://localhost:8000';
+	
 	var ProvidersDAO = {
 	    list: function() {
 		return $http({
 		    method: 'GET',
-		    url: '/api/providers'
+		    url: base + '/api/providers'
 		});
 	    },
 	    
 	    get: function(id) {
 		return $http({
 		    method: 'GET',
-		    url: '/api/providers/' + api
+		    url: base + '/api/providers/' + api
 		});
+	    },
+	    
+	    getHistory: function(providerId, historyId) {
+		if (historyId != null || historyId != undefined) {
+		    return $http({
+			method: 'GET',
+			url: base + '/api/providers/' + providerId + '/history/' + historyId
+		    });
+		} else {
+		    return $http({
+			method: 'GET',
+			url: base + '/api/providers/' + providerId + '/history'
+		    });
+		}
 	    }
 	}
 	
